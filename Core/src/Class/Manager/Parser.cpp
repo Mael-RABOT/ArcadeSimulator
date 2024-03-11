c146a8a60f2cd34264fa75bcd8aab89c8c6f3f63
Mael-RABOT <mael.rabot@gmail.com>
2024-03-11 11:08:07 +0100

    fix(cs): Make code coding style compliant
    Signed-off-by: Mael-RABOT <mael.rabot@gmail.com>

--- Core/src/Class/Manager/Parser.cpp
+++ Core/src/Class/Manager/Parser.cpp
@@ -1,10 +1,5 @@
 #include "../../../Class/Manager.hpp"
 
 namespace Core {
-    void Manager::Parser(int argc, char *argv[]) {
-        if (argc != 2) {
-            throw CoreError("Usage: ./arcade path_to_graphical_library");
-        }
-        this->loadGraphicalLibrary(argv[1]);
-    }
+    void Manager::Parser(int argc, char *argv[]) {if (argc != 2) {throw CoreError("Usage: ./arcade path_to_graphical_library");}this->loadGraphicalLibrary(argv[1]);}
 }
