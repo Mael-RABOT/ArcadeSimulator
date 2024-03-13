4994add67ec080986af8912b21425b391b9d815a
Mael-RABOT <mael.rabot@gmail.com>
2024-03-13 15:40:28 +0100

    wip
    Signed-off-by: Mael-RABOT <mael.rabot@gmail.com>

--- Core/src/main.cpp
+++ Core/src/main.cpp
@@ -5,8 +5,8 @@
 int main(int argc, char *argv[]) {
     CoreModule::Manager manager;
     try {
-        manager.Parser(argc, argv);
         manager.initLibSelectors();
+        manager.Parser(argc, argv);
     } catch (CoreModule::CoreError const &error) {
         std::cerr << error.what() << std::endl;
         return 84;
