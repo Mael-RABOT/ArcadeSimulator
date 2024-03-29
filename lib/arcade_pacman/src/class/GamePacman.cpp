2c4bcf5658a278b618ddb899c27556194d2de8b0
PERRIOLAT Nils <nils.perriolat@epitech.eu>
2024-03-29 13:05:54 -0400

    [ADD] add pacman lib to lib makefile and correcting bugs to compile

--- lib/arcade_pacman/src/class/GamePacman.cpp
+++ lib/arcade_pacman/src/class/GamePacman.cpp
@@ -2,43 +2,44 @@
 
 void GamePacman::handleInput(std::size_t deltaTime, Input input) {
     if (this->map.empty())
-        throw pacman::createError(pacman::Error::MAP_UNINITIALIZED);
+        throw pacman::quickError(pacman::Error::MAP_UNINITIALIZED);
 }
 
 void GamePacman::update(std::size_t deltaTime) {
     if (this->map.empty())
-        throw pacman::createError(pacman::Error::MAP_UNINITIALIZED);
+        throw pacman::quickError(pacman::Error::MAP_UNINITIALIZED);
 }
 
 std::vector<std::string> GamePacman::getInstruction() {
-    
+    std::vector<std::string> list = {std::string("")};
+    return list;
 }
 
-EntityDescription GamePacman::getEntities() {
-    return this->entityDescriptor;
+EntitiesDescription GamePacman::getEntities() {
+    return this->entitiesDescriptor;
 }
 
 Map GamePacman::getMap() {
     if (!this->map.empty())
         return this->map;
-    ifstream mapFile;
+    std::ifstream mapFile;
     mapFile.open ("assets/pacman_map.txt");
     if (!mapFile.is_open()) {
-        throw pacman::createError(pacman::Error::MAP_NOT_FOUND);
+        throw pacman::quickError(pacman::Error::MAP_NOT_FOUND);
     }
     std::string line;
     while(std::getline(mapFile, line)) {
-        if (!this->map.empty() && this->map.end().size() != line.length() - 1) {
-            throw pacman::createError(pacman::Error::MAP_CORRUPTED);
+        if (!this->map.empty() && this->map.end()->size() != line.length() - 1) {
+            throw pacman::quickError(pacman::Error::MAP_CORRUPTED);
         }
         std::vector<EntityType> mapLine;
         for (std::string::iterator c = line.begin(); c != line.end(); c++) {
-            if (c == '#') {
+            if (*c == '#') {
                 mapLine.push_back(WALL);
-            } else if (c == ' ') {
+            } else if (*c == ' ') {
                 mapLine.push_back(UNDEFINED);
-            } else if (c != '\n') {
-                throw pacman::createError(pacman::Error::MAP_CORRUPTED);
+            } else if (*c != '\n') {
+                throw pacman::quickError(pacman::Error::MAP_CORRUPTED);
             }
         }
         this->map.push_back(mapLine);
@@ -47,40 +48,34 @@ Map GamePacman::getMap() {
     return this->map;
 }
 
-std::map<EntityType, std::string> GamePacman::getSpriteDict() {
-    std::map<EntityType, std::string> dict = {
-        {WALL, std::string("wall.png")}
-    }
+std::map<EntityType, std::pair<std::string, std::size_t>> GamePacman::getSpriteDict() {
+    std::map<EntityType, std::pair<std::string, std::size_t>> dict = {
+        {WALL, {std::string("pacman_wall.png"), 0}}
+    };
     return dict;
 }
 
 std::map<StaticScreen, std::string> GamePacman::getStaticScreen() {
-    std::map<EntityType, std::string> dict = {
-    }
+    std::map<StaticScreen, std::string> dict = {
+    };
     return dict;
 }
 
-namespace pacman {
-    const char *Error::what(void) const noexcept {
-        switch (this->_type) {
-            case pacman::Error::MAP_NOT_FOUND:
-                return "PACMAN : ERROR : assets/pacman_map.txt file not found.";
-            case pacman::Error::MAP_CORRUPTED:
-                return "PACMAN : ERROR : assets/pacman_map.txt file is corrupted.";
-            case pacman::Error::MAP_UNINITIALIZED:
-                return "PACMAN : ERROR : the map has not been initialized.";
-            default:
-                return "PACMAN : ERROR : Undifined error";
-        }
+pacman::Error pacman::quickError(const Error::ErrorType_t type) {
+    std::string message;
+    switch (type) {
+        case pacman::Error::MAP_NOT_FOUND:
+            message = std::string("PACMAN : ERROR : assets/pacman_map.txt file not found.");
+        break;
+        case pacman::Error::MAP_CORRUPTED:
+            message = std::string("PACMAN : ERROR : assets/pacman_map.txt file is corrupted.");
+        break;
+        case pacman::Error::MAP_UNINITIALIZED:
+            message = std::string("PACMAN : ERROR : the map has not been initialized.");
+        break;
+        default:
+            message = std::string("PACMAN : ERROR : Undifined error");
     }
-
-    void Error::setErrorType(const Error::ErrorType_t type) {
-        this->_type = type;
-    }
-
-    Error createError(const Error::ErrorType_t type) {
-        pacman::Error err;
-        err.setErrorType(type);
-        return err;
-    }
+    pacman::Error err(message);
+    return err;
 }
--- lib/arcade_pacman/src/class/GamePacman.hpp
+++ lib/arcade_pacman/src/class/GamePacman.hpp
@@ -1,10 +1,9 @@
 #ifndef PACMAN_GAME_LIB
     #define PACMAN_GAME_LIB
 
-    #include <exception>
-    #include <functional>
     #include <fstream>
 
+    #include "ArcadeException.hpp"
     #include "IGameModule.hpp"
 
 class GamePacman : public IGameModule {
@@ -18,29 +17,26 @@ class GamePacman : public IGameModule {
         void update(std::size_t deltaTime) override;
 
         std::vector<std::string> getInstruction() override;
-        EntityDescription getEntities() override;
+        EntitiesDescription getEntities() override;
 
         Map getMap() override;
-        std::map<EntityType, std::string> getSpriteDict() override;
+        std::map<EntityType, std::pair<std::string, std::size_t>> getSpriteDict() override;
         std::map<StaticScreen, std::string> getStaticScreen() override;
 };
 
 namespace pacman {
-    class Error : public std::exception {
+    class Error : public ArcadeException {
         public:
             typedef enum ErrorType {
                 UNKNOWN,
                 MAP_NOT_FOUND,
-                MAP_CORRUPTED
+                MAP_CORRUPTED,
                 MAP_UNINITIALIZED
             } ErrorType_t;
 
-            const char *what(void) const noexcept override;
-            void setErrorType(const Error::ErrorType_t type);
-        private:
-            ErrorType_t _type;
+            Error(const std::string &message) : ArcadeException(message) {};
     };
 
-    Error createError(const Error::ErrorType_t type);
+    Error quickError(const Error::ErrorType_t type);
 }
 #endif
